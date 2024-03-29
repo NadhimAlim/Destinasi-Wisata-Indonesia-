11b56c48cd105c8159c4ab4eadef0bdac285964b
NadhimAlim <125029244+NadhimAlim@users.noreply.github.com>
2024-03-29 15:28:49 +0700

    Add files via upload

---
+++ footer/footer.php
@@ -0,0 +1,49 @@
+<footer>
+    <div class="containerfooter">
+        <div class="boxfooter">
+            <div class="logo">
+                <h1>Destinasi Wisata</h1>
+            </div>
+            <div class="terms">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+                    impedit cum cumque velit libero officiis quam doloremque
+                    reprehenderit quae corporis! Delectus architecto officia
+                    praesentium atque laudantium, nam deleniti sapiente deserunt.
+                </p>
+            </div>
+        </div>
+
+        <div class="boxfooter">
+            <h2>Destinasi</h2>
+            <a href="#">Wisata Alam</a>
+            <a href="#">Pariwisata Budaya</a>
+            <a href="#">Kuliner</a>
+            <a href="#">Paket Perjalanan</a>
+
+        </div>
+
+        <div class="boxfooter">
+            <h2>Informasi</h2>
+            <a href="#">Tentang Kami</a>
+            <a href="#">Lowongan</a>
+            <a href="#">Berita Terkini</a>
+            <a href="#">Layanan</a>
+        </div>
+
+        <div class="boxfooter">
+            <h2>Sosial Media</h2>
+            <a href="#">instagram</a>
+            <a href="#">Youtube</a>
+            <a href="#">twitter</a>
+        </div>
+    </div>
+
+    <div class="boxcopyright">
+        <hr />
+        <p>
+            Segala hak cipta dilindungi &copy; 2024 Destinasi Wisata. Dilarang
+            keras penggunaan tanpa izin
+        </p>
+    </div>
+</footer>
---
+++ kuliner/kuliner.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Destinasi Wisata</title>
+    <link rel="stylesheet" href="../css/styleutama.css" />
+    <script src="https://unpkg.com/feather-icons"></script>
+  </head>
+  <body>
+    <?php include "../navbar/navbar.php" ?>
+
+    <section id="hero" class="hero">
+      <video autoplay muted loop class="video-background">
+        <source src="../assets/wonderful.mp4" type="video/mp4" />
+      </video>
+      <main>
+        <h1>Kulineran Bersama Kami</h1>
+        <p>ini halaman wisata</p>
+      </main>
+    </section>
+
+    <h2 class="teamkamu">OUR TEAM</h2>
+
+    <section>
+        <h1>halo ini kuliner</h1>
+    </section>
+
+    <?php include "../footer/footer.php" ?>
+
+    <script src="./js/script.js"></script>
+
+    <script>
+      feather.replace();
+    </script>
+  </body>
+</html>
